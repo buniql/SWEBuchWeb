49112033e481e814a541c6452df1bbf77f9f225f
buniql <bunertlukas@gmail.com>
2023-05-28 15:56:18 +0200

    feat: added graphql codegen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import BasicTable from "./components/basictable";
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -29,14 +30,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <BasicTable />
       </div>
 
       <div className={styles.grid}>
@@ -91,5 +85,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }

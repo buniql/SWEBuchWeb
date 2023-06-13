673b394e0ab19aa0d7647ed0ed7aa64a626c033a
buniql <bunertlukas@gmail.com>
2023-06-13 22:29:33 +0200

    feat: added more routing

---
+++ src/app/mediacard/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import styles from "../page.module.css";
+import SearchBoxWithDataTable from "../view_model/SearchBoxWithDataTable";
+import ResponsiveAppBar from "../view_model/ResponsiveAppBar";
+import { useState } from "react";
+import SearchBoxWithMediaCards from "../view_model/SearchBoxWithMediaCards";
+
+export default function Home() {
+  const initPage = (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <ResponsiveAppBar />
+      </div>
+
+      <div className={styles.main}>
+        <SearchBoxWithMediaCards />
+      </div>
+    </main>
+  );
+
+  const [loggedIn, logIn] = useState<boolean>(false);
+
+  const [content, setContent] = useState<any>(initPage);
+  return content;
+}

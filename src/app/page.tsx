53afbf74adeee43e2e6f88ddcd47bf0e38c90025
buniql <bunertlukas@gmail.com>
2023-06-13 22:57:19 +0200

    fix: removed unwanted logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ResponsiveAppBar from "./view_model/ResponsiveAppBar";
 import SearchBoxWithMediaCards from "./view_model/SearchBoxWithMediaCards";
 
 export default function Home({ data }: any) {
-  console.log(data);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
